6337f9a4a4d0903aa7212244fde6fbaf00d6b261
Sambhav Saxena <76242518+sambhavsaxena@users.noreply.github.com>
2022-01-14 12:27:06 +0530

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 cd /usr/local/src/
-wget https://github.com/web3j/web3j/releases/download/v3.2.0/web3j-3.2.0.zip
-unzip web3j-3.2.0.zip
-
-./web3j-3.2.0/bin/web3j
+wget https://github.com/web3j/web3j/releases/download/v0.1.0/reactive-0.1.0.zip
+unzip reactive-0.1.0.zip
