7c988b3fb91a3b0026fb123b127cbeea56c69f8d
Sambhav Saxena <76242518+sambhavsaxena@users.noreply.github.com>
2023-01-15 17:25:13 +0530

    Update next-sitemap.js

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 const isProduction = process.env.NODE_ENV === 'production';
-const domain = isProduction ? 'nuro.dev' : 'localhost:3000';
+const domain = isProduction ? 'interpreted.vercel.app' : 'localhost:3000';
 const protocol = isProduction ? 'https' : 'http';
 
 /**
