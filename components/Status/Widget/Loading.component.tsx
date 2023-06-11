657b98cfab7dfcb111a8e3a888600f97ebf47c14
Sambhav Saxena <sambhavsaxena02@gmail.com>
2023-06-12 00:08:37 +0530

    fixed repeated style 2.0

--- components/Status/Widget/Loading.component.tsx
+++ components/Status/Widget/Loading.component.tsx
@@ -8,7 +8,7 @@ interface LoadingTypes {
 
 export function Loading({ type }: LoadingTypes) {
 	return (
-		<div className="flex space-x-4 w-full max-w-sm mx-auto px-4 py-4 bg-white bg-opacity-50 dark:(bg-gray-900 bg-opacity-50 border-gray-600) backdrop-filter backdrop-blur-sm border-2 border-gray-200 rounded-lg hover:shadow-lg default-transition motion-safe:animate-pulse">
+		<div className="flex space-x-4 w-full max-w-sm mx-auto px-4 py-4 bg-white bg-opacity-50 dark:(bg-gray-900 border-gray-600) backdrop-filter backdrop-blur-sm border-2 border-gray-200 rounded-lg hover:shadow-lg default-transition motion-safe:animate-pulse">
 			<div
 				className={clsx(
 					'w-12 h-12 my-auto bg-gray-200 dark:bg-gray-600',
