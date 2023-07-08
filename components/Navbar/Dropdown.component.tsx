6bcc9d6f6beee3d5828ffb1afa6f1522d0564668
Sambhav Saxena <sambhavsaxena02@gmail.com>
2023-07-08 07:38:30 +0530

    deleted 🗑️: latest blog component

--- components/Navbar/Dropdown.component.tsx
+++ components/Navbar/Dropdown.component.tsx
@@ -35,7 +35,7 @@ const StyledMenuItem = forwardRef<any, MenuLinkProps>(function StyledMenuItem(
 			className={clsx(
 				'flex items-center px-4 py-3 text-sm font-medium tracking-wide cursor-pointer default-transition',
 				active
-					? 'bg-gray-100 bg-opacity-50 text-gray-900 dark:(bg-gray-700 bg-opacity-50 text-white)'
+					? 'bg-gray-100 bg-opacity-50 text-gray-900 dark:(bg-gray-700 text-white)'
 					: 'text-gray-400 hover:text-gray-700 dark:hover:text-white',
 				className,
 			)}
