811e9561184161e56f2fceae2761a5690f9da8a9
Sambhav Saxena <76242518+sambhavsaxena@users.noreply.github.com>
2023-08-06 13:14:24 +0530

    fix: updated CV url

--- lib/navigation.tsx
+++ lib/navigation.tsx
@@ -57,7 +57,7 @@ const staticMenuItems: Array<Array<NavigationItem>> = [
 			type: NavigationItemType.LINK,
 			icon: 'feather:file-text',
 			text: 'Resume',
-			href: 'https://cdn.statically.io/gh/thatsameguyokay/images/main/CV.pdf',
+			href: 'https://cdn.statically.io/gh/thatsameguyokay/images/main/CV-main.pdf',
 			external: true,
 		},
 	],
