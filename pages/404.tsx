71e25d7fbb3ab52099ab460f27f2dbac91b381f3
Sambhav Saxena <sambhavsaxena02@gmail.com>
2023-09-01 13:11:10 +0530

    update: integrated colleagues into 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -16,12 +16,28 @@ export default function Error() {
 					</div>
 					<div className="py-4 text-center">
 						<h1 className="mt-2 text-4xl font-extrabold text-gray-500 dark:text-white tracking-tight sm:text-5xl">
-							Whoops!
+							Woah!
 						</h1>
 						<p className="mt-8 text-sm font-medium text-gray-400 dark:text-gray-300">
-							Looks like you took a wrong turn.
+							What have you done?! <br />
+							Enlightenment is not that easy to achieve. <br />
+						</p>
+						<p className="mt-8 text-sm font-medium text-gray-400 dark:text-gray-300">
+							"कर्मजं बुद्धियुक्ता हि फलं त्यक्त्वा मनीषिणः। जन्मबन्धविनिर्मुक्ताः पदं
+							गच्छन्त्यनामयम्॥" २-५१
 							<br />
-							The page you&apos;re looking for couldn&apos;t be found.
+						</p>
+						<p className="mt-8 text-sm font-medium text-gray-400 dark:text-gray-300">
+							The wise, possessed of knowledge, having abandoned the fruits of their
+							actions, and being freed from the fetters of birth, go to the place
+							which is beyond all evil.
+							<br />
+						</p>
+						<p className="mt-8 text-sm font-medium text-gray-400 dark:text-gray-300">
+							~Shri Krishna, Bhagavad Gita
+						</p>
+						<p className="mt-8 text-sm font-medium text-gray-400 dark:text-gray-300">
+							Here's what you can do now: <br />
 						</p>
 						<div className="mt-6 flex justify-center items-center space-x-4">
 							<Button.Standard
@@ -29,6 +45,12 @@ export default function Error() {
 								onClick={() => history.go(-1)}
 								icon="feather:arrow-left">
 								Back
+							</Button.Standard>
+							<Button.Standard
+								type={NavigationItemType.LINK}
+								href="/colleagues"
+								icon="mdi:handshake">
+								Colleagues
 							</Button.Standard>
 							<Button.Standard
 								type={NavigationItemType.LINK}
